ddc905862305ef6f803ffdfe4d32cd530f8b99a0
Lucas Bonan <lucasbonan15@gmail.com>
2023-12-04 17:49:37 -0300

    limpando o resultInput

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ document.querySelectorAll(".charKey").forEach(function (charKeyBtn) {
 
 document.getElementById("clear").addEventListener("click", function () {
   input.value = "";
+  resultInput.value = "";
   input.focus();
 });
 
